fe2908699e5cf666dac6a46ca3265d4a05dbf9fd
andryellemaria-dev <andryelle.maria@escolar.ifrn.edu.br>
2026-05-06 19:19:19 +0000

    Aula 06/05 - Introdução a JavaScript

---
+++ PPI26.js
@@ -0,0 +1,35 @@
+/* 
+Programação a internet
+*/
+// Introdução ao JavaScript
+console.log("Bem vindo a PPI!");
+
+// Variáveis
+let name = "João";
+let age = 17;
+let isStudent = true;
+// O isStudent já vai indicar que é uma variável booleana
+
+const PI = 3.14; // Não pode mudar de valor
+console.log(PI);
+
+// Operadores Aritméticos + - / * ** %
+let x = 5 + 5; // Soma
+let y = "5" + 5; // Concatenação
+let z = "Hello" + 5;
+
+console.log(x, y, z);
+console.log(typeof x);
+
+// Ativar o formatador Prettier
+// ALL + SHIFT + F
+
+// Operadores relacionais
+console.log(x != 2); // Operador de diferença
+console.log("5" == 5); // Compara tipo OU valor
+console.log("5" === 5); // Compara tipo E valor
+
+// Operadores de incremento
+console.log(x++); // Incremento após o retorno do valor
+console.log(x); 
+console.log(++x);
